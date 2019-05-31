0930e2f62a6bf04262ab0d1565c48e3d78f530ab
Claire Cassidy <clacassidy@gmail.com>
2019-05-31 16:50:18 -0700

    created sorted-list function

---
+++ src/sort-list.js
@@ -0,0 +1,16 @@
+function sortList(people, sortOptions) {
+    const property = sortOptions.property;
+    const direction = sortOptions.direction;
+
+    return people.slice().sort((a, b) => {
+        if(a[property] > b[property]) {
+            return 1 * direction;
+        }
+        else if(b[property] > a[property]) {
+            return -1 * direction;
+        }
+        return 0;
+    });
+}
+
+export default sortList;
